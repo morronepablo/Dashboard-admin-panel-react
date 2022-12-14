0dd49e2a3c14ece644c4d810a76b59ee9d637c43
Pablo Morrone <pmorrone@it.nortia.com.ar>
2022-12-14 11:07:57 -0300

    finalizacion del proyecto

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Navbar.scss'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+import { DarkModeContext } from '../../context/darkModeContext';
 
 const Navbar = () => {
+
+  const {dispatch} = useContext(DarkModeContext);
+  const {darkMode} = useContext(DarkModeContext);
+
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder='Search...' />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className='icon' />
+            English
+          </div>
+          <div className="item">
+            {!darkMode ? 
+              <DarkModeOutlinedIcon 
+                className='icon' 
+                onClick={() => {
+                  dispatch({type: 'TOGGLE'})
+                }} 
+                style={{cursor: 'pointer'}}
+              /> : 
+              <LightbulbOutlinedIcon 
+                className='icon' 
+                onClick={() => {
+                  dispatch({type: 'TOGGLE'})
+                }} 
+                style={{cursor: 'pointer'}}
+              />
+            }
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className='icon' />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className='icon' />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className='icon' />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className='icon' />
+          </div>
+          <div className="item">
+            <img
+                src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt=""
+                className="avatar"
+              />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
